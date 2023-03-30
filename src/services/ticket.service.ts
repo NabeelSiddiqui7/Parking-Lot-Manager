7f0ea45510c01093441b5def2fec6b69d21008d8
Saud Rehman <saudrehman00@hotmail.com>
2023-03-30 15:26:21 -0400

    fix get ticket service

--- src/services/ticket.service.ts
+++ src/services/ticket.service.ts
@@ -3,11 +3,11 @@ import prisma from "@databases/postgresClient"
 class TicketService {
     
     public async getTicket(ticketID: number) {
-        const results = await prisma.$queryRaw`SELECT * FROM lots`;
+        const results = await prisma.$queryRaw`SELECT id, spaceid, effectivedate, expirydate FROM tickets t WHERE t.id = ${ticketID}`;
         return results;
     }
     public async insertTicket(spaceID: number, expectedexpirydate: Date) {
-        const results = await prisma.$queryRaw`SELECT * FROM lots`;
+        const results = await prisma.$executeRaw`SELECT * FROM lots`;
         return results;
     }
 }
